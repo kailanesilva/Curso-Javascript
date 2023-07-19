5e4cdfc3f8fb2b4bbd2eceb7ec1aca68ef014a3f
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2023-07-19 16:44:36 -0300

    fiz uma prática

---
+++ exerciciosGitHub/teste/teste-4/script.js
@@ -0,0 +1,20 @@
+let res = document.getElementById('res')
+
+function calcular(){
+    let nome = prompt(`Qual é o nome do aluno?`)
+    let n1 = Number(prompt(`Qual foi a primeira nota de ${nome}?`))
+    let n2 = Number(prompt(`Além de ${n1}, qual foi a segunda nota de ${nome}?`))
+    let media = (n1 + n2) / 2
+    let msg 
+
+    if(media > 6){
+        msg = 'Meus parabéns!'
+    }else{
+        msg = 'Estude um pouco mais!'
+    }
+
+    res.innerHTML = `<p>Calculando a média final de <mark>${nome}</mark>.</p><br>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${n1}</mark> e <mark>${n2}</mark>.</p><br>`
+    res.innerHTML += `<p>A média final será <mark>${media}</mark>.</p><br>`
+    res.innerHTML += `<p>A mensagem que temos é: <strong style='color:red '>${msg}</strong></p><br>`
+}
