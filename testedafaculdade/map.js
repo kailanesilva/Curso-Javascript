ca9ecbb7fadff0af3c87937650a001cc27a20706
Kailane Silva <139505464+kailanesilva@users.noreply.github.com>
2023-11-18 12:33:44 -0300

    testes da faculdade

---
+++ testedafaculdade/map.js
@@ -0,0 +1,9 @@
+var cores = [
+    {id: 1, nome: 'Amarelo'},
+    {id: 2, nome: 'Verde'},
+    {id: 3, nome: 'Azul'},
+    {id: 4, nome: 'Branco'},
+    {id: 5, nome: 'Preto'}
+]
+const coresNomes = cores.map(cor => cor.nome);
+console.log(coresNomes);
